16437156f202750ce5e1c04f3ff2c56277093946
Mariusz Michalski <m.michalski@binarapps.com>
2023-11-29 13:47:32 +0100

    chore: basic layout

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AppHeader from './components/AppHeader.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+</script>
+
+<template>
+  <AppHeader />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
